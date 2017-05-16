371b219c341e8c01da61c46723f0cf80b406b98a
yl-zeng <ylinzeng69@gmail.com>
2017-05-16 14:48:08 -0400

    filter todo

--- app/api/TodoApi.jsx
+++ app/api/TodoApi.jsx
@@ -24,5 +24,28 @@ module.exports={
     }else{
       return [];
     }
+  },
+
+  filterTodos: function(todos,showCompleted,searchText){
+    var filteredTodos = todos;
+
+    filteredTodos = filteredTodos.filter((todo)=>{
+      return !todo.completed || showCompleted;
+    });
+
+    filteredTodos.sort((a,b)=>{
+      if(!a.completed && b.completed){
+        return -1;
+      }else if(a.completed && !b.completed){
+        return 1;
+      }else{
+        return 0;
+      }
+    });
+
+
+    return filteredTodos;
   }
+
+
 }
--- app/tests/api/TodoApi.test.jsx
+++ app/tests/api/TodoApi.test.jsx
@@ -59,6 +59,38 @@ describe("TodoApi",()=>{
 
   });
 
+  describe("filterTodos",()=>{
+    var todos =[{
+      id:1,
+      text:"test text 1",
+      completed:true
+    },{
+      id:2,
+      text:"test text 2",
+      completed:false
+    },{
+      id:3,
+      text:"test text 3",
+      completed:true
+    }];
+
+    it("should return all items if showCompleted is true",()=>{
+      var filteredTodos = TodoApi.filterTodos(todos,true,"");
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it("should return non-completed items if showCompleted is false",()=>{
+      var filteredTodos = TodoApi.filterTodos(todos,false,"");
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it("should sort by completed status",()=>{
+      var filteredTodos = TodoApi.filterTodos(todos,true,"");
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+  });
+
 
 
 
